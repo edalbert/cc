47808ba1676bebdc47ff6539a6784d804456e8c8
Ed Escalante <edalbert.escalante@gmail.com>
2022-11-09 12:58:47 +0800

    Install Faker gem and seed database for guests and reservations
    https://cake-olivine-4a0.notion.site/Seed-Database-a36766c5f33642268e6f80c3bea2e7e0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+15.times do
+  guest_attributes = {
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.unique.email,
+  }
+  guest = Guest.create(guest_attributes)
+
+  reservation_attributes = {
+    code: Faker::Alphanumeric.unique.alphanumeric(number: 11),
+    description: Faker::Company.bs,
+    end_date: '17-Oct-2024',
+    host_currency: 'AUD',
+    number_of_adults: 2,
+    number_of_children: 0,
+    number_of_guests: 2,
+    number_of_infants: 0,
+    number_of_nights: 7,
+    payout_price: 10_000,
+    security_price: 10_000,
+    start_date: '10-Oct-2024',
+    status: 'accepted',
+    total_price: 20_000,
+    guest_id: guest.id
+  }
+  Reservation.create(reservation_attributes)
+end
