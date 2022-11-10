73c677ab5be5afb62e9a429cde42fb1bc6098d14
Ed Escalante <edalbert.escalante+eabe@gmail.com>
2022-11-11 02:36:58 +0800

    Create action for reservations and add route POST /reservations
    https://cake-olivine-4a0.notion.site/Create-controller-and-routes-for-reservations-e3bf34d631f24bc9abe9b85c3de6db41

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,15 @@
 class ReservationsController < ApplicationController
+  wrap_parameters false
+
   def index
     @reservations = Reservation.all
 
     render json: @reservations
+  end
+
+  def create
+    @reservation = request.request_parameters
+
+    render json: @reservation
   end
 end
