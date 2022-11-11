6a9901bce951cf36af835fb6b54a91cc6fac963d
Ed Escalante <edalbert.escalante+eabe@gmail.com>
2022-11-11 21:38:56 +0800

    Add dependent: :destroy to Guest and GuestContact

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -16,8 +16,8 @@
 class Guest < ApplicationRecord
   validates :email, uniqueness: true
 
-  has_many :guest_contacts
-  has_many :reservations
+  has_many :guest_contacts, dependent: :destroy
+  has_many :reservations, dependent: :destroy
 
   accepts_nested_attributes_for :guest_contacts
 end
